636531b797a05be4d11be4e9445f374b297acca5
chanshik <chanshik@gmail.com>
2014-01-10 15:19:35 +0900

    Start implement MediaLibrary class.

---
+++ medialibrary.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+
+import os
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+import mutagen
+
+
+class MediaLibrary(object):
+    def __init__(self):
+        pass
+
+    def open_directory(self, dir_path):
+        pass
